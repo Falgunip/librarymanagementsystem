0818de070cc7098df69cde2e19422b88e3e07705
Falgunip <63282954+Falgunip@users.noreply.github.com>
2020-11-14 11:37:45 +0530

    Add files via upload

---
+++ library/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+from .models import *
+
+# Register your models here.
+admin.site.register(Users)
+admin.site.register(Admin)
+admin.site.register(Genres)
+admin.site.register(Books)
+admin.site.register(Issues)
+
+
+"username - falguhardi07"
+"password - ymmB6BQ8"
